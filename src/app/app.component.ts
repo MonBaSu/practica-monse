2014dc314b54c85864a3058493b9413af1c78b66
MonBaSu <monse.barrera.s3@gmail.com>
2023-06-19 10:33:38 -0600

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { YouTubePlayerModule } from "@angular/youtube-player";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+ 
+})
+export class AppComponent {
+  //title = 'TransDigital';
+  arreglo = ['Administración de servicios TI, recursos informáticos y contratos de servicios en sede principal y 5 subsedes (INTRANET)',
+    'Consolidación de implementación del ERP BAAN V',
+    "Diseño y construcción de los MDF's de la INTRANET",
+    'Migración de la operación crítica de Data Center Comercial'];
+
+}
+
+
+
